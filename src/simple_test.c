441ed039ead91a6bc9e506935919ef4f8c67ae76
BenYoo-ben <neverknown0909@gmail.com>
2021-08-05 10:10:08 +0900

    preparing lib change

---
+++ src/simple_test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <json.h>
+ 
+//Code is from:
+//https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=yababies&logNo=220029067066
+
+int main(int argc, char **argv)
+{
+    
+   printf("size of json_obj : %u\n",sizeof(struct json_object));
+}
--- src/test2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <json-c/json_object.h>
-
-int main(void)
-{
-	struct 	json_object* joi;
-	printf("TT.\n");
-	
-}
